d0a077a866a1ffa9128089b2556718b7a7c1deee
TranQuangLoc <tranquangloc2906@gmail.com>
2018-01-14 15:40:30 +0700

    commit APP_TAIXE giao dien

---
+++ Sources/DienThoai_APP/APP_TAIXE/Scripts/FX0001.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module('myApp', []);
+
+app.controller('myCtrl', function ($scope, $interval) {
+    $scope.manv = sessionStorage.manv;
+    $scope.tennv = sessionStorage.tennv;
+    $scope.thongtinxe = sessionStorage.xe;
+    $scope.trangthai = sessionStorage.mota;
+    $scope.lat = sessionStorage.lat;
+    $scope.lng = sessionStorage.lng;
+    $scope.id_xe = sessionStorage.idXe;
+
+    $scope.SDT = 'Không có thông tin';
+    $scope.DC = 'Không có thông tin';
+
+    $scope.f_setPoints = function (sdt, dc) {
+        $scope.SDT = sdt;
+        $scope.DC = dc;
+    }
+})
